ced4d34bdc5c5f39db91344f9fbb9466f94f7dfc
Creeperman16487 <abhiramardra@outlook.com>
2024-07-07 21:59:04 +0800

    added rna

--- src/main/java/net/Creeperman16487/extinct/item/ModItems.java
+++ src/main/java/net/Creeperman16487/extinct/item/ModItems.java
@@ -14,6 +14,9 @@ public class ModItems {
     public static final RegistryObject<Item> DNA = ITEMS.register("dna",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> RNA = ITEMS.register("rna",
+            () -> new Item(new Item.Properties()));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
