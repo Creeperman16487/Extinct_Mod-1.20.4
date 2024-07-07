8682901ea0f2be9524d4e9b19d452aaa094c5fd7
Creeperman16487 <abhiramardra@outlook.com>
2024-07-07 21:49:34 +0800

    added dna

--- src/main/java/net/Creeperman16487/extinct/ExtinctMod.java
+++ src/main/java/net/Creeperman16487/extinct/ExtinctMod.java
@@ -1,6 +1,9 @@
 package net.Creeperman16487.extinct;
 
 import com.mojang.logging.LogUtils;
+import net.Creeperman16487.extinct.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -24,6 +27,7 @@ public class ExtinctMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -41,6 +45,10 @@ public class ExtinctMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.DNA);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/Creeperman16487/extinct/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.Creeperman16487.extinct.item;
+
+import net.Creeperman16487.extinct.ExtinctMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ExtinctMod.MOD_ID);
+
+    public static final RegistryObject<Item> DNA = ITEMS.register("dna",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
